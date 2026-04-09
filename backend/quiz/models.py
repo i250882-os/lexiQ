8d18dfe1640637f5764b447ec277d59f687f6408
i250882-os <i250882@isb.nu.edu.pk>
2026-04-09 06:26:08 +0500

    Recovered Lost Files

--- backend/quiz/models.py
+++ backend/quiz/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+# Quiz -> Id, Questions, options, correct option
+
+
+class Quiz(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name="questions")
+    text = models.CharField(max_length=255)
+
+class Option(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="options")
+    text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
--- backend/quiz/views.py
+++ backend/quiz/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Response, status
+from words.models import UserWord
+from .models import Quiz
+from .serializers import GenerateQuizSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+### Quiz actions
+#- Quiz starts -> POST /quiz/start/
+#- Quiz ends -> POST /quiz/end/
+#- Mcq solved -> POST /quiz/mcq/solve
+#- Quiz generates -> POST /quiz/generate
+
+class GenerateQuizView(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        print("CALLED")
+        user = request.user
+        # if not user.is_authenticated:
+        #     return JsonResponse({'detail': 'Authentication required'}, status=401)
+        words = UserWord.objects.filter(user=user).select_related('word')
+        data = [
+            {
+                'word': w.word.text,
+                'status': w.status,
+                'score': w.score,
+            }
+            for w in words
+        ]
+        print("Words", data)
+        quiz = Quiz.objects.create(user=user)
+        quiz.save()
+        print(quiz.id)
+        return Response({'id': quiz.id, 'words': data}, status=status.HTTP_201_CREATED)
+
+class StartQuiz(APIView):
+    def post(self, request):
+       ... 
+
+class EndQuiz(APIView):
+    def post(self, request):
+        ...
+
+class McqSolved(APIView):
+    def post(self, request):
+        ...
